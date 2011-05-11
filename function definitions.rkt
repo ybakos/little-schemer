6d399aa4454b2a74b3b04e6d7d8fe52be6799b5a
Yong Bakos <ybakos@humanoriented.com>
2011-05-11 14:53:42 -0600

    Adding member? definition to Racket env.

--- function definitions.rkt
+++ function definitions.rkt
@@ -9,3 +9,10 @@
       ((null? l) #t)
       ((atom? (car l)) (lat? (cdr l)))
       (else #f))))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
