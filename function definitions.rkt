689cccc655dddf5a44a3c4e0fa946b09d775acff
Yong Bakos <ybakos@humanoriented.com>
2011-05-16 12:59:51 -0600

    Adding rember to Racket functions.

--- function definitions.rkt
+++ function definitions.rkt
@@ -16,3 +16,10 @@
       ((null? lat) #f)
       (else (or (eq? (car lat) a)
                 (member? a (cdr lat)))))))
+
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? (car lat) a) (cdr lat))
+      (else (cons (car lat) (rember a (cdr lat)))))))
