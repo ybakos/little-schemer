5d441c895c8926bce4b5f415ed15bbc31a228d5c
Yong Bakos <ybakos@humanoriented.com>
2011-05-18 13:21:04 -0600

    Adding subst function.

--- function definitions.rkt
+++ function definitions.rkt
@@ -71,3 +71,17 @@
   )
 )
 
+;;; Replaces the first occurance of old in lat with new.
+(define subst
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? (car lat) old) (cons new (cdr lat)))
+          (else (cons (car lat) (subst new old (cdr lat))))
+        )
+      )
+    )
+  )
+)
