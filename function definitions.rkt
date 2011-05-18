2e21ffb1a30cd51dbd8402a22f6033677e068262
Yong Bakos <ybakos@humanoriented.com>
2011-05-18 13:29:19 -0600

    Adding subst2 function.

--- function definitions.rkt
+++ function definitions.rkt
@@ -85,3 +85,18 @@
     )
   )
 )
+
+;;; Replaces the first occurance of old in lat with new.
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((or (eq? (car lat) o1) (eq? (car lat) o2)) (cons new (cdr lat)))
+          (else (cons (car lat) (subst2 new o1 o2 (cdr lat))))
+        )
+      )
+    )
+  )
+)
