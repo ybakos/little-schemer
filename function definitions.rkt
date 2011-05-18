862493f2dad5dd632f2c2ab002c5ee47955bfb17
Yong Bakos <ybakos@humanoriented.com>
2011-05-18 13:53:33 -0600

    Ignoring additional racket files. Adding function multiinsertL

--- function definitions.rkt
+++ function definitions.rkt
@@ -123,3 +123,18 @@
     )
   )
 )
+
+;;; Inserts the atom new to the left of all occurences of old in list lat.
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? (car lat) old) (cons new (cons old (multiinsertL new old (cdr lat)))))
+          (else (cons (car lat) (multiinsertL new old (cdr lat))))
+        )
+      )
+    )
+  )
+)
