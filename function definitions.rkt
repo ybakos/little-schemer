3897ea2a7f55b0d925716006eaaeb2aec2d89448
Yong Bakos <ybakos@humanoriented.com>
2011-05-18 13:07:19 -0600

    Adding firsts and insertR from chapter 3.

--- function definitions.rkt
+++ function definitions.rkt
@@ -23,3 +23,26 @@
       ((null? lat) '())
       ((eq? (car lat) a) (cdr lat))
       (else (cons (car lat) (rember a (cdr lat)))))))
+
+(define firsts
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (car (car l)) (firsts (cdr l))))
+    )
+  )
+)
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+      ((null? lat) '())
+      (else
+        (cond
+          ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+          (else (cons (car lat) (insertR new old (cdr lat))))
+        )
+      )
+    )
+  )
+)
